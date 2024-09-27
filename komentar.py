7d34d8870af0ccfdecaaf27b635aae70c844d88a
aisyahartikasari06 <aisyahas627@gmail.com>
2024-09-27 17:30:36 +0700

    Add files via upload

---
+++ komentar.py
@@ -0,0 +1,7 @@
+# variabel a memepresentasikan panjang
+a = 5 
+b = 10 # variabel b mempresentasikan tinggi
+"""
+dan variabel c mempresentasikan luas persegi dari hasil perkalian variabela dan variabel b
+"""
+c = a * b  
