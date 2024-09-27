0b597b12485a653c3e71cdf74f65b8f47deea189
aisyahartikasari06 <aisyahas627@gmail.com>
2024-09-27 17:06:07 +0700

    Add files via upload

---
+++ tipedata.string.py
@@ -0,0 +1,13 @@
+nama_depan = "aisyah"
+nama_belakang = 'artika'
+nama_lengkap = nama_depan + '' + nama_belakang 
+usia = '18'
+alamat = 'belitung'
+kata_mutiara = "ambil resikonya atau kamu hanya akan gagal"
+
+print(nama_lengkap, '(' + usia +')',',','dari',alamat,',kata mutiara:',kata_mutiara) 
+
+print('\ntipe dari nama_lengkap:', type(nama_lengkap))
+print('tipe dari usia:', type (usia))
+print('tipe dari alamat:', type(alamat))
+print('tipe dari kata_mutiara:', type(kata_mutiara)) 
