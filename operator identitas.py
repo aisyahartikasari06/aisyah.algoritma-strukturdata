1273f818ce8776f1bf452911471e15b5dff25dd2
aisyahartikasari06 <aisyahas627@gmail.com>
2024-10-11 09:55:00 +0700

    Add files via upload

---
+++ operator identitas.py
@@ -0,0 +1,14 @@
+a = 5
+b = 5
+nama_a = 'budi'
+nama_b = 'budi'
+# Output True 
+print('a is b:', a is b)
+# Output False
+print('a is not b:', a is not b)
+# Output True 
+print('nama_a is nama_b:', nama_a is nama_b)
+# Output False
+print('nama_a is not nama_b:', nama_a is not nama_b)
+
+
