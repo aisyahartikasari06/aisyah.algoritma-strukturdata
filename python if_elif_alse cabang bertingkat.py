eb0106bc5e17cb35e55394dd094698e16dcbd172
aisyah <aisyahas627@gmail.com>
2024-11-29 23:52:28 +0700

    Add files via upload

---
+++ python if_elif_alse cabang bertingkat.py
@@ -0,0 +1,12 @@
+nilai = int(input('masukan nilai anda :'))
+usia = int(input('masukan usia anda :'))
+if nilai >= 75:
+    if (usia < 15):
+        print('selamat adek, kamu lulus!')
+    else:
+        print('selamat kakak, kamu lulus!')
+else:
+    if(usia < 15):
+        print('mohon maaf dek, kamu tidak lulus!')
+    else:
+        print('mohon maaf kakak, kamu tidak lulus!')
