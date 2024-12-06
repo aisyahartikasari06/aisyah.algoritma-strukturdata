c550f960cb86c4955ec2637650c070b53c5682ca
aisyah <aisyahas627@gmail.com>
2024-12-06 10:05:28 +0700

    Add files via upload

---
+++ pemrograman while list kota.py
@@ -0,0 +1,10 @@
+listkota = [
+    'Jakarta', 'Surabaya', 'Depok', 'Bekasi', 'Solo', 
+    'Jogjakarta', 'Semaranag', 'Makasar'
+]
+
+# bermain index
+i = 0
+while i < len(listkota):
+    print(listkota[i])
+    i += 1
