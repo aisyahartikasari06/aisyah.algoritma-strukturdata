2d7d99e6196926a334826331e81ede625ac7fa57
aisyah <aisyahas627@gmail.com>
2024-12-27 10:41:51 +0700

    Add files via upload

---
+++ INPUT USIA UAS.py
@@ -0,0 +1,11 @@
+usia = (int(input('Masukan usia anda :')))
+if usia <= 5:
+    print('usia anda adalah balita')
+elif usia <= 12:
+    print('usia anda adalah anak-anak')
+elif usia <= 17:
+    print('usia anda adalah remaja')
+elif usia <= 59:
+    print('usia anda adalah dewasa')
+else:
+    print('usia anda adalah lansia')
